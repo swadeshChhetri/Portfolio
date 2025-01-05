ca442849a17c71216bdc8e849fde3658624244ca
Swadesh Chhetri <swadeshchhetri87@gmail.com>
2025-01-05 14:47:08 +0530

    update about section

--- src/components/about/aboutSection.jsx
+++ src/components/about/aboutSection.jsx
@@ -23,16 +23,8 @@ const AboutSection = () => {
             data-aos-duration="1500"
           >
             <p>
-              <h3><strong>Hi, I'm Swadesh</strong></h3> <h3 style={{marginBottom:'15px'}}><strong>Frontend Developer | Bengaluru, India</strong></h3>I specialize
-              in creating responsive and dynamic web applications using a range
-              of technologies including <strong> HTML, CSS, JavaScript, Bootstrap, React,
-              Redux, Java,</strong> and <b> Git</b> for version control. I'm passionate about
-              continuously improving my coding skills and developing modern,
-              user-friendly websites. With a strong focus on front-end
-              development, I take pride in building visually appealing and
-              functional web applications. I’m confident in my ability to meet
-              project goals and am eager to contribute to innovative IT
-              companies.
+              <h3><strong>Hi, I'm Swadesh</strong></h3> <h3 style={{marginBottom:'15px'}}><strong>Frontend Developer | Bengaluru, India</strong></h3>
+              I am a passionate and hardworking professional with experience in Full-Stack Web Development. I enjoy building user-friendly and efficient solutions using technologies like HTML, CSS, JavaScript, and modern frameworks. I have worked on developing a company portal, which reflects my ability to manage projects effectively and deliver impactful results. Additionally, I gained valuable industry experience through a Full-Stack web development internship with a Tamil Nadu-based company. I thrive in team environments where I can collaborate and bring creative ideas to life. My goal is to create meaningful projects that solve real-world problems and add value to businesses.
             </p>
             <a  className="btnn" href="https://drive.google.com/file/d/1U9qOeWDVS3J1uPXoeDO80cJ3yVYBRNfj/view?usp=drive_link">Resume</a>
             
